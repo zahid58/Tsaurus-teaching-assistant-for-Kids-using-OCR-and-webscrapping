2ee331763098f1b1fdf1a279a3b94b68913f89e1
Md. Zahidul Islam <32902848+iut-160041010@users.noreply.github.com>
2019-04-20 16:56:33 +0600

    sqlite is fully added

--- main.js
+++ main.js
@@ -58,110 +58,165 @@ app.on('activate', function () {
 
 //***************************************
 
-
 //this function in typedInput.html
 function processWordTypedInput()
-{   
+{
+  console.log("in processWordTypedInput---"); 
   var word=document.getElementById("input1").value;
-  localStorage.setItem("word",word);
-  callPython();
+  localStorage.setItem("word",word); 
+  callOutput(word);
 }
+
 function processWordOcrInput(word)
 {
+  console.log("in processWordOcrInput---");
   localStorage.setItem("word",word);
-  callPython();
+  callOutput(word);   
 }
 
 
 //call processWord.py
-function callPython()
+async function callOutput(word)
 {
-  
+  console.log("in callOutput---");
   const loader = document.getElementById("loader");
   loader.className += " show";
+  console.log(loader.className);
+  console.log("loader showing...");
+  var db=require('better-sqlite3')('G:/PROJECTS/SoftDevProject/tsauruswithsqlite/temp/sqlite2.db');
+
+  var row = await checkDatabase(db); // first check whether data is already in database or not.
+  console.log(row);
+  if(typeof row == 'undefined') // if no data found in database.
+  {
+    console.log("calling python");
+    await callPython(db); // call python , save data in database, send a copy to js and show output in html.
+  } 
+  else{  // if data was not found we directly show the data in output.
+    console.log("no need to call python");
+    showOutput(db);
+  }
+}
+
+async function checkDatabase(db){
+ 
+  console.log("in async  function check database...")
+  var row = undefined;
   var word = localStorage.getItem("word");
+  console.log(word);
+  try{
+    db.prepare("CREATE TABLE IF NOT EXISTS words (word text primary key,dic_text  text,speech_type text,description text,img_url1 text,img_url2 text,img_url3 text,img_url4 text,img_url5 text,img_url6 text,freq_of_srch number);");
+    row = db.prepare('select * from words where word=?;').get(word);
+    console.log(row);
+    console.log("i am here!"); 
+  }
+  catch(err){
+    console.log("database error : %j",err);
+  }
+  return row;
+}
+
+async function callPython(db){
+
+  console.log("in async function callPython...")
+  var word = localStorage.getItem("word");
+  console.log(word)
   const {PythonShell} = require("python-shell");
   const path = require("path");
   const script_path = path.join(__dirname,'../backend/');
   let options = {
     mode: 'text',
     pythonPath: "G:/anaconda3/python.exe",
-    scriptPath: "G:/PROJECTS/SoftDevProject/tsaurusElectron/backend",
+    scriptPath: "G:/PROJECTS/SoftDevProject/tsauruswithsqlite/backend",
     args: [word]
   }
   console.log(options);
-  let pyWord = new PythonShell('processWord.py',options) ;
-  pyWord.on('message',function(message){
-    location.replace("output.html");
-  })
-}
+  
+  PythonShell.run('processWord.py', options, await  async function (err, results) {
+    if (err) throw err;
+    // results is an array consisting of messages collected during execution
+    console.log('results: %j', results);
+    var row = JSON.parse(results[0]);
+    console.log(row);
+    await processOutput(row);
+    const output = document.getElementById("output");
+    output.className += " show";
+  });
 
-
-
-//this function in OUTPUT.HTML
-function processOutput()
-{
-  document.getElementById("suppliedWord").innerHTML = localStorage.getItem("word");
-  localStorage.removeItem("word");
-  var dictionaryString ="";
-  var encyclopediaString = "";
-  //need to make these above variables all scope
-  const fs = require("fs");
-  var filepath = "temp/dictionary.txt";
-  try{
-    dictionaryString = fs.readFileSync(filepath, 'utf-8');
-    document.getElementById("dictionary").innerHTML += dictionaryString;
-  }
-  catch(err)
-  {
-    alert(err);
-  }
-  filepath = "temp/description.txt";
-  try{
-    encyclopediaString = fs.readFileSync(filepath,'utf-8');
-    document.getElementById("encyclopedia").innerHTML += encyclopediaString;
-  }
-  catch(err)
-  {
-    alert(err);
-  }
+  console.log("done with python")
   
 }
 
-function loadOutputImages()
+var word;
+var row;
+
+async function showOutput(db)
 {
-  const fs = require("fs");
-  var filepath = "temp/imageurls.json";
-  try{
-    var imageurls = JSON.parse(fs.readFileSync(filepath));
-    imageurls = Object.keys(imageurls)
-    
-    
-    for(var i=0;i<imageurls.length;i++)
-    {
-      var x = document.createElement("IMG");
-      x.setAttribute("src", imageurls[i]);
-      x.setAttribute("width", "200");
-      x.setAttribute("height","140");
-      x.setAttribute("style","margin:4px;");
-      document.getElementById("images_").appendChild(x);
-    }
-  }
-  catch(err)
+  var a = "something";
+  console.log("something");
+  console.log("in showOutput async function...");
+  console.log(db);
+  word = localStorage.getItem("word");
+  console.log(word);
+  row  = db.prepare('select * from words where word=?;').get(word);
+  console.log(row);
+  if( typeof row == 'undefined')
   {
-     alert(err);
+    processOutputError();
+    const output = document.getElementById("output");
+    output.className += " show";
+  }
+  else{
+    processOutput(row);
+    const output = document.getElementById("output");
+    output.className += " show";
   }
 }
 
 
+// error output 
+async function processOutputError(){
+  console.log("in processOutputError---");
+  document.getElementById("suppliedWord").innerHTML = localStorage.getItem("word");
+  localStorage.removeItem("word");
+  document.getElementById("dictionary").innerHTML+= "Check your Internet Connection !!!"
+  document.getElementById("encyclopedia").innerHTML+= "Ooops! Mr.Tsaurus could not reach the net !!! ";
+  for(var i=0;i<6;i++)
+  {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../temp/errorImage.jpg");
+    x.setAttribute("width", "200");
+    x.setAttribute("height","140");
+    x.setAttribute("style","margin:4px;");
+    document.getElementById("images_").appendChild(x);
+  }
+}
+
+// optimum output
+async function processOutput(row)
+{
+  console.log("in processOutput---");
+  document.getElementById("suppliedWord").innerHTML = row.word;
+  localStorage.removeItem("word");
+  document.getElementById("dictionary").innerHTML+=row.dic_text;
+  document.getElementById("encyclopedia").innerHTML+=row.description;
+  var imageurls   = [ row.img_url1, row.img_url2, row.img_url3, row.img_url4, row.img_url5, row.img_url6 ];  
+  for(var i=0;i<imageurls.length;i++)
+  {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", imageurls[i]);
+    x.setAttribute("width", "200");
+    x.setAttribute("height","140");
+    x.setAttribute("style","margin:4px;");
+    document.getElementById("images_").appendChild(x);
+  }
+
+}
+
 
 function callDetect(){
-// call the python OCR program
-// go to a page thats supposed to run when ocr runs
-// then call ocr
-// wait for message
-// load ocrInput.html 
-// hopefully it will work
+
+  console.log("in call detect...");
   const loader = document.getElementById("loader");
   loader.className += " show";
   const {PythonShell} = require("python-shell");
@@ -170,17 +225,21 @@ function callDetect(){
   let options = {
     mode: 'text',
     pythonPath: "G:/anaconda3/python.exe",
-    scriptPath: "G:/PROJECTS/SoftDevProject/tsaurusElectron/backend",
+    scriptPath: "G:/PROJECTS/SoftDevProject/tsauruswithsqlite/backend",
   }
   console.log(options);
   let pyWord = new PythonShell('ocrInput.py',options) ;
   pyWord.on('message',function(message){
     location.replace("ocrInput.html");
   })
+
 }
 
+
+//called from ocrInput.html
 function getDetectedWords()
 {
+  console.log("in get detected words...");
   const fs = require("fs");
   var filepath = "temp/detected.json";
   try{
@@ -200,16 +259,47 @@ function getDetectedWords()
   }
   catch(err)
   {
-    //create a text saying ..sorry i could not detect anything..
-     alert(err);
+    document.getElementById("menu").innerHTML = "Oops, Mr. Tsaurus could not detect anything from your Camera!";
+    alert(err);
   }
 }
 
 
 
+//_____________________________________________________
+//          our code ends here!
+//-----------------------------------------------------
+//
+//_____________________________________________________
 /*imageurls.forEach(function(item){
       
     });*/
+
+/* 
+    function loadOutputImages()
+    {
+      const fs = require("fs");
+      var filepath = "temp/imageurls.json";
+      try{
+        var imageurls = JSON.parse(fs.readFileSync(filepath));
+        imageurls = Object.keys(imageurls)
+        
+        
+        for(var i=0;i<imageurls.length;i++)
+        {
+          var x = document.createElement("IMG");
+          x.setAttribute("src", imageurls[i]);
+          x.setAttribute("width", "200");
+          x.setAttribute("height","140");
+          x.setAttribute("style","margin:4px;");
+          document.getElementById("images_").appendChild(x);
+        }
+      }
+      catch(err)
+      {
+         alert(err);
+      }
+    }
 
 /*
 python path --
@@ -238,3 +328,53 @@ G:\anaconda3\lib\site-packages\Pythonwin
     </p1>
 </div>
 */
+
+/*
+
+  const db = require('better-sqlite3')('G:/PROJECTS/SoftDevProject/tsauruswithsqlite/temp/sqlite2.db');
+  var row = undefined;
+  try{
+    row = db.prepare('select * from word_table where word=?;').get(word);
+    console.log(row);
+  }
+  catch{
+    db.prepare("CREATE TABLE IF NOT EXISTS word_table (word text primary key,dic_text  text,speech_type text,description text,img_url1 text,img_url2 text,img_url3 text,img_url4 text,img_url5 text,img_url6 text,freq_of_srch number);");
+    row = db.prepare('select * from word_table where word =?;').get(word);
+    console.log(row);
+  }
+
+  if(typeof row == 'undefined')
+  {
+    
+    var word = localStorage.getItem("word");
+    const {PythonShell} = require("python-shell");
+    const path = require("path");
+    const script_path = path.join(__dirname,'../backend/');
+    let options = {
+      mode: 'text',
+      pythonPath: "G:/anaconda3/python.exe",
+      scriptPath: "G:/PROJECTS/SoftDevProject/tsaurusElectron/backend",
+      args: [word]
+    }
+    console.log(options);
+    let pyWord = new PythonShell('processWord.py',options) ;
+    
+    var status = false;
+
+    pyWord.on('message',function(message){
+        console.log(message);
+        status = true;
+        one();
+    })
+
+*/
+
+/*let pyWord = new PythonShell('processWord.py',options) ;
+  pyWord.on('message',function(message){
+    console.log(message);
+  });
+  pyWord.end( function(err){
+    console.log(err); 
+    }
+  )
+  */
